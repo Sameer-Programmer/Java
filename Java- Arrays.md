80ed4b5604e4dd4ba72c722fbaf803a89ed0dfa9
sameer-Programmer <sameeridea7@gmail.com>
2023-06-24 12:29:44 +0530

    s

--- Java- Arrays.md
+++ Java- Arrays.md
@@ -1,2 +1,6 @@
 # Arrays
 Array is a colection of elements of same datatype is called Arrays
+
+Declaration of Array 
+int a[] = new a[6];      // Normal 
+int a[] = new a{1,2,3,4,5};  //  Declaration in shortform
--- example/Test22.java
+++ example/Test22.java
@@ -1,3 +1,11 @@
 public class Test22 {
-    
+    Test22(int a , int b){
+        int c = a+b;
+        System.out.println(c+  "          Operation through Constructor");
+    }
+
+    public static void main(String[] args) {
+        Test22 test22 = new Test22(1,2);
+        
+    }
 }
