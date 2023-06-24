0d16e6c6e03bcba732347e78dcf057dabdec6e24
sameer-Programmer <sameeridea7@gmail.com>
2023-06-24 13:07:17 +0530

    abstract

---
+++ example/Test24.java
@@ -0,0 +1,17 @@
+abstract class Tiger{
+    public abstract void eat();
+    
+}
+class BabyTiger extends Tiger{
+    @Override
+    public void eat() {
+        System.out.println("Baby tiger");
+    };
+    }
+public class Test24 {
+    public static void main(String[] args) {
+      BabyTiger babyTiger = new BabyTiger();
+      babyTiger.eat();
+       
+    }
+}
