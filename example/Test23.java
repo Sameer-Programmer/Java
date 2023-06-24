69c6e3ee1d222f2200225f523f10df476c9e5f19
sameer-Programmer <sameeridea7@gmail.com>
2023-06-24 12:38:39 +0530

    constructor

---
+++ example/Test23.java
@@ -0,0 +1,14 @@
+public class Test23 {
+    int a =10;
+    int b = 30;
+    Test23 (){
+        this.a = 23;
+        this.b = 24;
+        int sum = a+b;
+        System.out.println(sum +"             from constructor ");
+        }
+        public static void main(String[] args) {
+            Test23 test23 = new Test23();
+    }
+}
+
