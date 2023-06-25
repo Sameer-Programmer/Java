8717f410df6543eb48ecadc8708eb95b482b80c9
sameer-Programmer <sameeridea7@gmail.com>
2023-06-25 13:13:03 +0530

    cash

--- example/Test29.java
+++ example/Test29.java
@@ -1,18 +1,18 @@
 class Encapsulation {
-    private  int currency;
-    public void getcurrency(){
-        System.out.println("getting Currency");
+    private  int cash;
+    public void getcash(){
+        System.out.println("getting cash");
     }
-    public int setcurrency(int currency){
-        this.currency = currency;
-        return currency; 
+    public int setcash(int cash){
+        this.cash = cash;
+        return cash; 
     }
 }
     
 public class Test29 {
     public static void main(String[] args) {
         Encapsulation encapsulation = new Encapsulation();
-        encapsulation.getcurrency();
-        System.out.println(encapsulation.setcurrency(2000));
+        encapsulation.getcash();
+        System.out.println(encapsulation.setcash(2000));
     }
 }
