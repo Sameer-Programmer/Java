fe68697ed423bd975daf8a84de83bbd2608a347b
sameer-Programmer <sameeridea7@gmail.com>
2023-06-25 12:05:45 +0530

    Encapsulation

---
+++ example/Test28.java
@@ -0,0 +1,19 @@
+class Bank {
+    private int currency;
+
+    public void getcurrency(){
+        System.out.println("from getter");
+    }
+    public int  seta(int currency){
+        return currency;
+    }
+}
+
+public class Test28 {
+    public static void main(String[] args) {
+        Bank b = new Bank();
+        b.getcurrency();
+        System.out.println(b.seta(200));
+    }
+
+}
