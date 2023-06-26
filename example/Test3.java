4d1dc3f7f124b7b048683f34d2bb281010fec5a3
Sameer-Programmer <115461857+Sameer-Programmer@users.noreply.github.com>
2023-06-26 10:11:56 +0000

    e

--- example/Test3.java
+++ example/Test3.java
@@ -1,4 +1,4 @@
-package org.example;
+//package org.example;
 
 public class Test3 {
     public static void main(String[] args) {
--- example/Test4.java
+++ example/Test4.java
@@ -1,4 +1,4 @@
-package org.example;
+//package org.example;
 
 public class Test4 {
     public static void main(String[] args) {
