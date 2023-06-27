47933c0d1f9aff49e5db63469d5fc1690a057646
sameer-Programmer <sameeridea7@gmail.com>
2023-06-27 11:36:56 +0530

    Casting , conversion and Type Promotion

---
+++ example/Test34.java
@@ -0,0 +1,19 @@
+public class Test34 {
+    public static void main(String[] args) {
+        // Convertion
+        byte b = 20;
+        int  c = b;
+        System.out.println(c+"          conversion");
+
+        // Casting
+        int Boat = 100;
+        byte ship = (byte)Boat;
+        System.out.println(ship +"        Casting");
+
+        // Type Promotion
+        byte bb = 22;
+        byte cc =33;
+        int sum = bb+cc;
+        System.out.println(sum +"               Type Promotion");
+    }
+}
