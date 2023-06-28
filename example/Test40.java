c8a09836ad83a40080a5f79c610285785e6dd7b3
sameer-Programmer <sameeridea7@gmail.com>
2023-06-28 13:29:22 +0530

    run time poly

--- example/Test40.java
+++ example/Test40.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class Test40 {
+
+//Agenda to implement in Method -Overiding - Run time Polymorphism
+//Parent class reference variable refers to Object of child class
+
+class Apollo {
+   public double m1(){
+        return 0;
+    }
+}
+class YMG extends Apollo {
+    public double m1(){
+        return 1000;
+    }
+}
+class AD extends Apollo {
+    public double m1(){
+        return 4000;
+    }
+}
+class Kurnool extends Apollo {
+    public double m1(){
+        return 4000;
+    }
+}
+
+public class Test40 {
+    public static void main(String[] args) {
+        Apollo ap;
+        ap = new YMG();  System.out.println(ap.m1());
+        ap = new AD();  System.out.println(ap.m1());
+        ap = new Kurnool();  System.out.println(ap.m1());
+    }
 }
