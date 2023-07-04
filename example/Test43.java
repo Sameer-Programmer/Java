835eff987614eeb6d54454e92bced78dcc78884f
sameer-Programmer <sameeridea7@gmail.com>
2023-07-04 12:45:07 +0530

    p

--- example/Test43.java
+++ example/Test43.java
@@ -1,3 +1,21 @@
-public class Test43 {
+class ISV {
+    double Property = 4.20;
+}
 
+class Ak extends ISV {
+    public void m1(double Property){
+
+        System.out.println(Property +super.Property+"   TOTAL AK Property            ");
+    }
+}
+
+class MBD extends  Ak {
+
+}
+
+public class Test43 {
+    public static void main(String[] args) {
+       MBD mb = new MBD();
+       mb.m1(30.80);
+    }
 }
