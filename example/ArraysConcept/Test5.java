a3e09a4fab9322fd1b6e62aa1b380242e31b3280
sameer <sameeridea7@gmail.com>
2023-07-05 22:35:38 +0530

    list

---
+++ example/ArraysConcept/Test5.java
@@ -0,0 +1,13 @@
+package ArraysConcept;
+
+import java.util.ArrayList;
+
+public class Test5 {
+    public static void main(String[] args) {
+        ArrayList list = new ArrayList<>();
+        list.add("sameer");
+        list.add ("imran");
+        System.out.println(list);
+        System.out.println(list.size());
+    }
+}
