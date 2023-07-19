d98e023dbb1931daf049e99e1b51d24b3cc5809c
sameer-Programmer <sameeridea7@gmail.com>
2023-07-19 18:07:54 +0530

    abcdefghkddlkl

--- example/Collection_Concept/Assignment9.java
+++ example/Collection_Concept/Assignment9.java
@@ -15,32 +15,35 @@ public class Assignment9 {
         hs.add('A');
         hs.add(true);
         hs.add(null);
-        System.out.println(hs);    // Elements will print in Random Order
+        System.out.println(hs+"                  hs-1");    // Elements will print in Random Order
         hs.remove(11); // we dont have Index Concept
-        System.out.println(hs);
+        System.out.println(hs+"                  hs-2");
+        hs.removeAll(hs);
+        System.out.println(hs+"                  removed");
         /// We dont have get method in hashset
         // Here we dont Update the Existing Value
 
-        System.out.println( hs.contains("Sameer"));
-        System.out.println( hs.contains(11));
-        System.out.println( hs.isEmpty());
+//        System.out.println( hs.contains("Sameer")+"                  hs-3");
+//        System.out.println( hs.contains(11)+"                  hs-4");
+//        System.out.println( hs.isEmpty()+"                  hs-5");
 
         // Lets Use For Loop ----- first take one Object
-        for (Object e : hs) {
-            System.out.println(e);
-        }
+//        for (Object e : hs) {
+//            System.out.println(e);
+//        }
 
 
         HashSet si = new HashSet();
         si.add(20);
         si.addAll(hs);               // add all method
-        System.out.println(si);
+        System.out.println(si+"                  hs-6");
 
         HashSet nh = new HashSet();
         nh.add(1000);
         nh.add(hs);
-        System.out.println(nh);    // Added one new Element , complete hashset element in oneMore Hashset-nh
-        System.out.println(nh.removeAll(hs));
+        System.out.println(nh+"                  hs-7");    // Added one new Element , complete hashset element in oneMore Hashset-nh
+        System.out.println(nh.removeAll(hs)+"                  hs-8"); // Remove all method return only true or false
+
 
 
 
