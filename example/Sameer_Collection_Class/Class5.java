ab30b48e4b5a0133559afed16449d9f12e528581
sameer <sameeridea7@gmail.com>
2023-07-23 13:45:09 +0530

    123456

--- example/Sameer_Collection_Class/Class5.java
+++ example/Sameer_Collection_Class/Class5.java
@@ -9,7 +9,7 @@ import java.util.HashSet;
 3.convert Hashset into Array List
 4.Convert Array List into Array
  */
-public class Class_4 {
+public class Class5 {
     public static void main(String[] args) {
         int a [] = {1, 1,2, 3, 4, 5, 6,6, 7, 8, 9, 10}; // Array
         // length of the array
@@ -18,18 +18,17 @@ public class Class_4 {
         for(int i =0; i<length; i++){
             System.out.println(a[i]);
         }
-
+        String s1 = Arrays.toString(a);
+        // Here whole array is becomes to single element string "1,1,2,3,4, 5, 6,6, 7, 8, 9, 10"
+        System.out.println(s1);
         // Convert array into ArrayList
         ArrayList list = new ArrayList<>();
-        for(int i =0; i<length; i++){
-            list.add (a[i]);         //Here we are adding array indexes to array List through method add
-        }
-        System.out.println(list+"            Arraylist");
+        list.add(s1);
+        System.out.println(list);
 
-
-        // Convert ArrayList  into  HashSet
         HashSet hs = new HashSet();
         hs.addAll(list);
-        System.out.println(hs+"            Hashset");
+        System.out.println(hs);
     }
 }
+
