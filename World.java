7abe981b6981005cc9ef7b6437c5084e21deefc0
Sameer-Programmer <115461857+Sameer-Programmer@users.noreply.github.com>
2023-09-02 17:44:41 +0530

    Add files via upload

---
+++ World.java
@@ -0,0 +1,17 @@
+public class World {
+public static void main (String[] args) {
+	System.out.println("Hello_World");
+}
+}
+  /*Explanation
+ 1.public --access-Modifier
+ 2.World -- class Name 
+ 3.static -- key Word
+ 4.void -- return type
+ 5.main ---method name 
+ 6.String[] args --- This is an array Name String  which pass command line Arguements 
+ 7.System--- System is a class of type Printstream
+ 8.out -- out is a static field or variable
+ 9.println --- is a method of PrintStream class
+ */
+ 
