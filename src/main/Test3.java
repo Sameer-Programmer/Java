28b818061f16185ad64b9b84b88add95713cb504
Sameer-Programmer <115461857+Sameer-Programmer@users.noreply.github.com>
2024-08-16 12:32:56 +0000

    c

--- src/main/Test3.java
+++ src/main/Test3.java
@@ -1,7 +1,7 @@
 public class Test3 {
     public static void main (){
         int a =10;
-        double b = 20 
+        double b = 20 ;
         double Total = a+b;
         System.out.println("Total"+ Total);
     }
